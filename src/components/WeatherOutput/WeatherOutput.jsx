9466edaf01e8072d4ac8b0bf9784e689f4862090
Vlad Khomenko <vladkhomenko@MacBook-Air-Vlad.local>
2023-02-24 22:04:50 +0200

    add fetch data from api

--- src/components/WeatherOutput/WeatherOutput.jsx
+++ src/components/WeatherOutput/WeatherOutput.jsx
@@ -1,12 +1,21 @@
 import { FaSun, FaCloud, FaCloudSun, FaCloudRain, FaSnowflake } from 'react-icons/fa';
 import axios from 'axios';
-
+import { useEffect } from 'react';
+import { useCityContext } from '../../helpers/city-context';
 const API_KEY = '243fd0876f4114a945f94ebbf4f04b79'
 
 
 const WeatherOutput = () => {
+    const {city, weather} = useCityContext();
+    useEffect(() => {
+        axios
+        .get(`https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`)
+        .then(response => console.log(response));
+    }, [])
+                     
     return (
         <div>WeatherOutput</div>
+
     )
 }
 export default WeatherOutput
